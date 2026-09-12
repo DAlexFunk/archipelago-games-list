19b304c9ab5eb77c6ca9cf7905176bfcf0b4e1d0
Alex Funk <alexdagreat04@gmail.com>
2026-09-12 18:38:58 -0400

    docs: add function docs

--- src/models.js
+++ src/models.js
@@ -10,6 +10,10 @@ const SHEETS_DATA_VALUES = Object.freeze({
 	DISCLOSURES: 7,
 });
 
+/**
+ * Gets the row data from the spreadsheet
+ * @returns {Promise<Object[]>} The rows of the spreadsheet
+ */
 async function getSheetData() {
 	const SHEET_ID = "1iuzDTOAvdoNe8Ne8i461qGNucg5OuEoF-Ikqs8aUQZw";
 	const SHEET_RANGE = "'Playable Worlds'!A:H";
@@ -28,6 +32,11 @@ async function getSheetData() {
 }
 
 // STEAM SECTION
+/**
+ * Normalizes the name of a game by removing special characters, accents, and whitespace and turns it lowercase
+ * @param {string} game Game name to be normalized
+ * @returns {string} Normalized game name
+ */
 function normalize(game) {
 	return game
 		.normalize("NFD")
@@ -36,14 +45,20 @@ function normalize(game) {
 		.replace(/[^a-z0-9]/g, "");
 }
 
+/**
+ * Fuzzy matches the names of 2 game to determine if they are the same
+ * @param {string} game1 The name of the first game
+ * @param {string} game2 The name of the second game
+ * @returns {boolean} Whether or not the names are considered the same
+ */
 function areGamesSame(game1, game2) {
 	const a = normalize(game1);
 	const b = normalize(game2);
 	if (a === b) return true;
 
 	// Fuzzy searching:
-	const short = a.length < b.lenght ? a : b;
-	const long = a.length < b.lenght ? b : a;
+	const short = a.length < b.length ? a : b;
+	const long = a.length < b.length ? b : a;
 
 	// Don't check super short substrings
 	if (short.length < 5) return false;
@@ -51,6 +66,11 @@ function areGamesSame(game1, game2) {
 	return long.includes(short);
 }
 
+/**
+ * Gets the names of the games a given steam user owns
+ * @param {string} steamid The 17-digit steam id of the account to get the games of
+ * @returns {Promise<string[]>} The names of the games that user owns
+ */
 async function getSteamGames(steamid) {
 	const raw_steam_games = await fetch(
 		`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_KEY}&steamid=${steamid}&format=json&include_appinfo=1&include_played_free_games=1`,
