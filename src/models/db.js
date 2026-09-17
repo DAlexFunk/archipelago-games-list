4b85288c8f0d5a53efc5c40e1be4435618901dcc
Alex Funk <alexdagreat04@gmail.com>
2026-09-17 16:39:18 -0400

    feat: add sorting on the games

--- src/models/db.js
+++ src/models/db.js
@@ -12,11 +12,30 @@ async function DB_connect() {
 }
 
 /**
- * Gets all of the games in the db
+ * Gets all of the games in the db sorted by stability then name
  * @returns {Promise<Object[]>} All of the games in the db
  */
 async function DB_getAllGames() {
-	return await archi_games.find({}).toArray();
+	const games = await archi_games.find({}).toArray();
+	return (
+		games
+			// Sorts in alphabetical order ignoring and that start with "A", "An", or "The"
+			.sort((game1, game2) => {
+				const remove_a_or_the_or_an = (name) => name.replace(/^(a|an|the)\s+/i, "");
+
+				return remove_a_or_the_or_an(game1.name).localeCompare(remove_a_or_the_or_an(game2.name));
+			})
+			// Sorts stability into Stable -> Unstable -> Broken on Main
+			.sort((game1, game2) => {
+				const games_map = {
+					Stable: 1,
+					Unstable: 2,
+					"Broken on Main": 3,
+				};
+
+				return games_map[game1.stability] - games_map[game2.stability];
+			})
+	);
 }
 
 /**
